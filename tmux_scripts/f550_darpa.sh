da26655fc9f06b5570759b55bb2e064930e1809e
uav10 <mrs@mrs.felk.cvut.cz>
2019-04-06 21:19:31 +0200

    Median filter for optic flow

--- tmux_scripts/f550_darpa.sh
+++ tmux_scripts/f550_darpa.sh
@@ -18,6 +18,8 @@ input=(
 '
   'MRS_control' 'waitForRos; roslaunch mrs_uav_manager f550_darpa.launch
 '
+  'Thrust' 'waitForOdometry; rostopic echo /uav10/mavros/setpoint_raw/target_attitude/thrust
+'
   'Bumper' 'waitForOdometry; roslaunch mrs_bumper bumper.launch
 '
   'orb_slam' 'waitForOdometry; roslaunch orb_slam uav_darpa.launch'
