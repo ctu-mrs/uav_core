e9fa5aea4d7752ce0f6f344bf07a7906f552a7a1
uav10 <mrs@mrs.felk.cvut.cz>
2019-04-07 04:48:53 +0200

    After successful night tunnel flight

--- tmux_scripts/f550_darpa.sh
+++ tmux_scripts/f550_darpa.sh
@@ -12,7 +12,7 @@ pre_input="export ATHAME_ENABLED=0; mkdir -p $MAIN_DIR/$PROJECT_NAME; export DIS
 input=(
   'Rosbag' 'waitForRos; roslaunch mrs_general record_stola_josef.launch project_name:='"$PROJECT_NAME"'
   '
-  'OptFlow' 'waitForRos; roslaunch mrs_optic_flow uav10_outdoor.launch
+  'OptFlow' 'waitForRos; roslaunch mrs_optic_flow uav10_dark.launch
 '
   'Sensors' 'waitForRos; roslaunch mrs_general sensors_darpa.launch
 '
