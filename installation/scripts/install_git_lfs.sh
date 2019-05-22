1eec825b35c6750406f89bd287aac557a35cbdb3
Robert Penicka <penicrob@fel.cvut.cz>
2019-05-22 21:42:05 +0200

    add -y to some install scripts

--- installation/scripts/install_git_lfs.sh
+++ installation/scripts/install_git_lfs.sh
@@ -2,8 +2,8 @@
 # author: Robert Penicka
 
 echo "Installing git Large File Stograge (LFS) support"
-sudo apt-get install software-properties-common
+sudo apt-get -y install software-properties-common
 sudo curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash
-sudo apt-get install git-lfs
+sudo apt-get -y install git-lfs
 git lfs install
 echo "done installing LFS"
