6a4b52368a7c62288d8c6a0d45ceb1a5ada89b71
Robert Penicka <penicrob@fel.cvut.cz>
2019-06-18 12:48:00 +0200

    minor implovement of testbuild

--- .test_build.sh
+++ .test_build.sh
@@ -19,10 +19,6 @@ gitman install --force
 cd "$UAV_CORE_PATH/installation/scripts"
 ./install_mavlink.sh
 
-sudo rosdep init
-sudo rosdep fix-permissions
-rosdep update
-
 # source installation/scripts/download_binaries.sh
 # echo "binaries downloaded and configured"
 cd $UAV_CORE_PATH
@@ -33,6 +29,11 @@ mkdir -p $ROS_WORKSPACE/src
 ln -s $UAV_CORE_PATH $ROS_WORKSPACE/src/uav_core
 cd $ROS_WORKSPACE
 source /opt/ros/melodic/setup.bash
+
+sudo rosdep init
+sudo rosdep fix-permissions
+rosdep update
+
 catkin init
 
 catkin config --profile debug --cmake-args -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS='-std=c++17 -march=native -fno-diagnostics-color' -DCMAKE_C_FLAGS='-march=native -fno-diagnostics-color'
