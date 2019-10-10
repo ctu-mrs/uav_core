8f113447c7a25ff107faf2cb331779eba2a7a071
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-10-10 11:48:39 +0200

    updated script for adding bashrc variables

--- miscellaneous/scripts/setup_rc_variables.sh
+++ miscellaneous/scripts/setup_rc_variables.sh
@@ -8,6 +8,7 @@ cd $MY_PATH
 # variable name, value, commentary
 input=(
   'UAV_NAME' 'uav1' ''
+  'NATO_NAME' '' 'lower-case name of the UAV frame {alpha, bravo, charlie, ...}'
   'UAV_MASS' '3.0' '[kg], used only with real UAV'
   'RUN_TYPE' 'simulation' '{simulation, uav}'
   'UAV_TYPE' 'f550' '{f550, f450, t650, eagle, naki}'
@@ -17,15 +18,16 @@ input=(
   'INITIAL_DISTURBANCE_Y' '0.0' '[N], external disturbance in the body frame'
   'STANDALONE' 'false' 'disables the core nodelete manager'
   'SWAP_GARMINS' 'false' 'swap up/down garmins'
+  'PIXGARM' 'false' 'true if Garmin lidar is connected throught Pixhawk'
   'SENSORS' '' '{garmin_down, garmin_up, rplidar, realsense_front, teraranger, bluefox_optflow}'
 )
 
 # create arrays
 for ((i=0; i < ${#input[*]}; i++));
 do
-  ((i%3==0)) && vars[$i/3]="${input[$i]}" 
-	((i%3==1)) && values[$i/3]="${input[$i]}"
-	((i%3==2)) && comments[$i/3]="${input[$i]}"
+  ((i%3==0)) && vars[$i/3]="${input[$i]}"
+  ((i%3==1)) && values[$i/3]="${input[$i]}"
+  ((i%3==2)) && comments[$i/3]="${input[$i]}"
 done
 
 for ((i=0; i < ((${#vars[*]})); i++));
