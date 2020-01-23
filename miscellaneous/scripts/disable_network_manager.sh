3cc1bbee3d7cfada4ed45eb368ba65ba548ca48d
uav60 <>
2020-01-23 07:07:41 +0100

    added script to disable fckin network manager (leave it to netplan to avoid ping problems etc)

---
+++ miscellaneous/scripts/disable_network_manager.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+sudo systemctl stop NetworkManager.service
+sudo systemctl disable NetworkManager.service
+
+sudo systemctl stop NetworkManager-wait-online.service
+sudo systemctl disable NetworkManager-wait-online.service
+
+sudo systemctl stop NetworkManager-dispatcher.service
+sudo systemctl disable NetworkManager-dispatcher.service
+
+sudo systemctl stop network-manager.service
+sudo systemctl disable network-manager.service
