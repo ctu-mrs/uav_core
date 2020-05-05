ab12ce10e07ee0c6ab0c7cab0d4d029ceb7b9d5a
Daniel Hert <hertdani@fel.cvut.cz>
2020-05-05 10:18:19 +0200

    added a script to fix network interface names to eth0/wlan0 etc.

---
+++ miscellaneous/scripts/fix_network_interface_names.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+sudo sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="net.ifnames=0 biosdevname=0"/g' /etc/default/grub
+sudo update-grub
+
+echo " "
+echo "Reboot the system now"
