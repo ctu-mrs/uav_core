d3367934fc030f6bb09101fdf35c6cac5bc90dab
Tomas Baca <klaxalk@gmail.com>
2020-05-05 11:17:56 +0200

    updated ros.sh

--- installation/dependencies/ros.sh
+++ installation/dependencies/ros.sh
@@ -10,6 +10,6 @@ echo "Installing ROS"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt -y update
-sudo apt -y install ros-melodic-desktop-full
+sudo apt -y install ros-melodic-ros-base
 
 exit 0
