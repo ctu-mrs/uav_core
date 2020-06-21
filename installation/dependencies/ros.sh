be7fbf8bdc00616377e965d0f068e6263f36b0ce
Tomas Baca <klaxalk@gmail.com>
2020-06-21 23:05:31 +0200

    updated ros.sh install script

--- installation/dependencies/ros.sh
+++ installation/dependencies/ros.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
 trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
 
@@ -24,11 +28,11 @@ sudo apt -y update
 [ "$distro" = "18.04" ] && sudo apt -y install ros-melodic-ros-base
 [ "$distro" = "20.04" ] && sudo apt -y install ros-noetic-ros-base
 
-num=`cat ~/.bashrc | grep "/opt/ros/melodic/setup.bash" | wc -l`
+num=`cat ~/.bashrc | grep "/opt/ros/$ROS_DISTRO/setup.bash" | wc -l`
 if [ "$num" -lt "1" ]; then
 
   # set bashrc
   echo "
-source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
 fi
