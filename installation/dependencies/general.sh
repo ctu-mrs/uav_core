98ac2a0867fe7187218b1d4989b1dc6f00a0050a
klaxalk <klaxalk@gmail.com>
2020-09-16 14:59:35 +0200

    fixing the gce... package freezing during installation

--- installation/dependencies/general.sh
+++ installation/dependencies/general.sh
@@ -16,6 +16,11 @@ sudo apt-get -y install cmake build-essential autotools-dev automake autoconf
 # utilities
 sudo apt -y install wget zip silversearcher-ag
 
+# the "gce-compute-image-packages" package often freezes the installation at some point
+# the installation freezes when it tries to manage some systemd services
+# this attempts to install the package and stop the problematic service during the process
+((sleep 90 && (sudo systemctl stop google-instance-setup.service && echo "gce service stoped" || echo "gce service not stoped")) & (sudo timeout 120s apt-get -y install gce-compute-image-packages)) || echo "\e[1;31mInstallation of gce-compute-image-packages failed\e[0m"
+
 # ros-related
 sudo apt-get -y install \
   ros-$ROS_DISTRO-visualization-msgs\
