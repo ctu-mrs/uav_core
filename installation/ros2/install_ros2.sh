bfda7ee7fe29e976a73c1782deb29a2f68053dfe
Tomas Baca <klaxalk@gmail.com>
2021-01-12 19:06:38 +0100

    added ros2 and ros1_bridge install scripts

---
+++ installation/ros2/install_ros2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+trap 'echo "$0: \"${last_command}\" command failed with exit code $?"' ERR
+
+echo "$0: Installing ROS2"
+
+# set locale to UTF-8
+sudo apt update && sudo apt install locales
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+sudo apt-get -y update && sudo apt install curl gnupg2 lsb-release
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://packages.ros.org/ros2/ubuntu $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+
+sudo apt-get -y update
+sudo apt-get -y install ros-foxy-desktop
+
+# command line autocompletion
+sudo apt-get -y install python3-pip
+pip3 install -U argcomplete
+
+# install colcon
+sudo apt install python3-colcon-common-extensions
