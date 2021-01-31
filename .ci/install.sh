cb859e5b9fdde8a2b6d14b189c68498c061e7c82
Tomas Baca <klaxalk@gmail.com>
2021-01-31 10:04:54 +0100

    added ACCEPT_EULA=Y

--- .ci/install.sh
+++ .ci/install.sh
@@ -5,6 +5,8 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 [ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
 [ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
 
+export ACCEPT_EULA=Y
+
 echo "Starting install"
 
 sudo apt-get -y update -qq
