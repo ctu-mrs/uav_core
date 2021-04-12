2d71b9ecdf1bc23e73d6cbd00ff0ba949d1f4b6d
Tomas Baca <klaxalk@gmail.com>
2021-04-12 09:28:44 +0400

    added swarming tmux template

---
+++ tmux_scripts/swarming_template/record.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+path="/home/\$(optenv USER mrs)/bag_files/latest/"
+
+# By default, we record everything.
+# Except for this list of EXCLUDED topics:
+exclude=(
+
+# IN GENERAL, DON'T RECORD CAMERAS
+#
+# If you want to record cameras, create a copy of this script
+# and place it at your tmux session.
+#
+# Please, seek an advice of a senior researcher of MRS about
+# what can be recorded. Recording too much data can lead to
+# ROS communication hiccups, which can lead to eland, failsafe
+# or just a CRASH.
+
+# Every topic containing "compressed"
+'(.*)compressed(.*)'
+# Every topic containing "image_raw"
+'(.*)image_raw(.*)'
+# Every topic containing "theora"
+'(.*)theora(.*)'
+# Every topic containing "h264"
+'(.*)h264(.*)'
+
+)
+
+# file's header
+filename=`mktemp`
+echo "<launch>" > "$filename"
+echo "<arg name=\"UAV_NAME\" default=\"\$(env UAV_NAME)\" />" >> "$filename"
+echo "<group ns=\"\$(arg UAV_NAME)\">" >> "$filename"
+
+echo -n "<node pkg=\"rosbag\" type=\"record\" name=\"rosbag_record\" args=\"-o $path -a" >> "$filename"
+
+# if there is anything to exclude
+if [ "${#exclude[*]}" -gt 0 ]; then
+
+  echo -n " -x " >> "$filename"
+
+  # list all the string and separate the with |
+  for ((i=0; i < ${#exclude[*]}; i++));
+  do
+    echo -n "${exclude[$i]}" >> "$filename"
+    if [ "$i" -lt "$( expr ${#exclude[*]} - 1)" ]; then
+      echo -n "|" >> "$filename"
+    fi
+  done
+
+fi
+
+echo "\" />" >> "$filename"
+
+# file's footer
+echo "</group>" >> "$filename"
+echo "</launch>" >> "$filename"
+
+cat $filename
+roslaunch $filename
