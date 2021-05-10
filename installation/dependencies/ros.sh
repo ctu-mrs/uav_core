63b47cab25de236a731e40933aacfc1a9b427dbf
Tomas Baca <klaxalk@gmail.com>
2021-05-10 15:54:27 +0200

    fix

--- installation/dependencies/ros.sh
+++ installation/dependencies/ros.sh
@@ -31,6 +31,7 @@ done
 
 sudo apt-get -y update
 
+sudo apt-get -y upgrade libgazebo9-dev
 sudo apt-get -y install libzmq3-dev
 
 [ "$distro" = "18.04" ] && sudo apt-get -y install ros-melodic-ros-base
