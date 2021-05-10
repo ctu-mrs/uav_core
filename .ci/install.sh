4c86fd142e480f5d1e259b29f264cd1d7a15d638
Tomas Baca <klaxalk@gmail.com>
2021-05-10 15:41:29 +0200

    updated ci install.sh

--- .ci/install.sh
+++ .ci/install.sh
@@ -7,19 +7,15 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 
 echo "Starting install"
 
-sudo apt-get -y install git
-
-echo "clone uav_core"
-cd
-git clone https://github.com/ctu-mrs/uav_core.git
-cd uav_core
+# get the path to this script
+MY_PATH=`pwd`
 
 echo "running the main install.sh"
 ./installation/install.sh
 
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-ln -s ~/uav_core
+ln -s "$MY_PATH"
 source /opt/ros/$ROS_DISTRO/setup.bash
 cd ~/catkin_ws
 
