f6d8391557e94991c6b77a8a48b7b6b1bc7afdd6
Tomas Baca <klaxalk@gmail.com>
2021-05-10 16:57:25 +0200

    fix

--- installation/dependencies/ros.sh
+++ installation/dependencies/ros.sh
@@ -32,8 +32,9 @@ done
 sudo apt-get -y update
 
 # FIXING ROS installation!!!! don't remove!!!
-sudo apt-get upgrade -y libignition-common3*
-[ "$distro" = "18.04" ] && sudo apt-get upgrade -y libgazebo9-dev
+sudo apt-get -y install --fix-broken
+# sudo apt-get upgrade -y libignition-common3*
+# [ "$distro" = "18.04" ] && sudo apt-get upgrade -y libgazebo9-dev
 
 [ "$distro" = "18.04" ] && sudo apt-get -y install ros-melodic-ros-base
 [ "$distro" = "20.04" ] && sudo apt-get -y install ros-noetic-ros-base
