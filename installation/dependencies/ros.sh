7577827a8459118e2f45727901575eee673583db
Tomáš Báča <tomas.baca@fel.cvut.cz>
2021-05-14 14:54:06 +0200

    Fixing CI
    rolled back ignition deb source, fixed CI for pull requests

--- installation/dependencies/ros.sh
+++ installation/dependencies/ros.sh
@@ -13,12 +13,6 @@ distro=`lsb_release -r | awk '{ print $2 }'`
 
 echo "$0: Installing ROS"
 
-# add repository for ignition library
-sudo apt-get -y install wget lsb-release gnupg
-sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu-stable `lsb_release -cs` main" > /etc/apt/sources.list.d/gazebo-stable.list'
-wget http://packages.osrfoundation.org/gazebo.key -O - | sudo apt-key add -
-sudo apt-get -y update
-
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 
 for server in ha.pool.sks-keyservers.net \
