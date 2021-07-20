5ef4a46893ff6b8414700d5d83b5e2c7182ac690
Tomas Baca <klaxalk@gmail.com>
2021-07-20 09:21:42 +0200

    fixed prefix-bug in the generate-ssh-config file

--- miscellaneous/dotssh/generate_ssh_config.sh
+++ miscellaneous/dotssh/generate_ssh_config.sh
@@ -3,62 +3,65 @@
 key_name="~/.ssh/id_rsa_drone_login"
 
 input=(
-  'vio' 'uav35' '192.168.69.135'
-  'alfa' 'uav60' '192.168.69.160'
-  'bravo' 'uav61' '192.168.69.161'
-  'charlie' 'uav62' '192.168.69.162'
-  'delta' 'uav63' '192.168.69.163'
-  'echo' 'uav64' '192.168.69.164'
-  'foxtrot' 'uav65' '192.168.69.165'
-  'golf' 'uav66' '192.168.69.166'
-  'hotel' 'uav67' '192.168.69.167'
-  'india' 'uav68' '192.168.69.168'
-  'juliett' 'uav90' '192.168.69.190'
-  'kilo' 'uav69' '192.168.69.169'
-  'lima' 'uav70' '192.168.69.170'
-  'mike' 'uav71' '192.168.69.171'
-  'oscar' 'uav46' '192.168.69.146'
-  'papa' 'uav45' '192.168.69.145'
-  'quebec' 'uav47' '192.168.69.147'
-  'romeo' 'uav49' '192.168.69.149'
-  'sierra' 'uav50' '192.168.69.150'
-  'tango' 'uav51' '192.168.69.151'
-  'uniform' 'uav42' '192.168.69.142'
-  'victor' 'uav53' '192.168.69.153'
-  'whiskey' 'uav44' '192.168.69.144'
-  'xray' 'uav55' '192.168.69.155'
-  'zulu' 'uav56' '192.168.69.156'
-  'xavier' 'uav99' '192.168.69.199'
-  'xavnx1' 'uav95' '192.168.69.195'
-  'eagle' 'uav91' '192.168.69.191'
-  'alice' 'uav1' '192.168.69.101'
-  'bob' 'uav2' '192.168.69.102'
-  'carol' 'uav3' '192.168.69.103'
-  'diego' 'uav4' '192.168.69.104'
-  'eric' 'uav5' '192.168.69.105'
-  'frank' 'uav6' '192.168.69.106'
-  'grace' 'uav7' '192.168.69.107'
-  'heidi' 'uav8' '192.168.69.108'
-  'ivan' 'uav9' '192.168.69.109'
-  'judy' 'uav10' '192.168.69.110'
-  'kyle' 'uav11' '192.168.69.111'
-  'lemmy' 'uav12' '192.168.69.112'
-  'max' 'uav13' '192.168.69.113'
-  'nick' 'uav14' '192.168.69.114'
-  'olivia' 'uav15' '192.168.69.115'
-  'pat' 'uav16' '192.168.69.116'
-  'quentin' 'uav17' '192.168.69.117'
-  'robert' 'uav18' '192.168.69.118'
-  'sophie' 'uav19' '192.168.69.119'
-  'trudy' 'uav20' '192.168.69.120'
-  'unique' 'uav21' '192.168.69.121'
-  'vanna' 'uav22' '192.168.69.122'
-  'walter' 'uav23' '192.168.69.123'
-  'dofec' 'uav80' '192.168.69.180'
-  'klaxalk' 'klaxalk-local' '192.168.69.11'
-  'dan' 'dan-local' '192.168.69.42'
-  'evzen' 'uav52' '192.168.69.152'
+  'alice'       'uav1'  '192.168.69.101'
+  'bob'         'uav2'  '192.168.69.102'
+  'carol'       'uav3'  '192.168.69.103'
+  'diego'       'uav4'  '192.168.69.104'
+  'eric'        'uav5'  '192.168.69.105'
+  'frank'       'uav6'  '192.168.69.106'
+  'grace'       'uav7'  '192.168.69.107'
+  'heidi'       'uav8'  '192.168.69.108'
+  'ivan'        'uav9'  '192.168.69.109'
+  'judy'        'uav10' '192.168.69.110'
+  'kyle'        'uav11' '192.168.69.111'
+  'lemmy'       'uav12' '192.168.69.112'
+  'max'         'uav13' '192.168.69.113'
+  'nick'        'uav14' '192.168.69.114'
+  'olivia'      'uav15' '192.168.69.115'
+  'pat'         'uav16' '192.168.69.116'
+  'quentin'     'uav17' '192.168.69.117'
+  'robert'      'uav18' '192.168.69.118'
+  'sophie'      'uav19' '192.168.69.119'
+  'trudy'       'uav20' '192.168.69.120'
+  'unique'      'uav21' '192.168.69.121'
+  'vanna'       'uav22' '192.168.69.122'
+  'walter'      'uav23' '192.168.69.123'
+  'vio'         'uav35' '192.168.69.135'
+  'uniform'     'uav42' '192.168.69.142'
+  'whiskey'     'uav44' '192.168.69.144'
+  'papa'        'uav45' '192.168.69.145'
+  'oscar'       'uav46' '192.168.69.146'
+  'quebec'      'uav47' '192.168.69.147'
+  'romeo'       'uav49' '192.168.69.149'
+  'sierra'      'uav50' '192.168.69.150'
+  'tango'       'uav51' '192.168.69.151'
+  'evzen'       'uav52' '192.168.69.152'
+  'victor'      'uav53' '192.168.69.153'
   'rododendron' 'uav54' '192.168.69.154'
+  'xray'        'uav55' '192.168.69.155'
+  'zulu'        'uav56' '192.168.69.156'
+  'alfa'        'uav60' '192.168.69.160'
+  'bravo'       'uav61' '192.168.69.161'
+  'charlie'     'uav62' '192.168.69.162'
+  'delta'       'uav63' '192.168.69.163'
+  'echo'        'uav64' '192.168.69.164'
+  'foxtrot'     'uav65' '192.168.69.165'
+  'golf'        'uav66' '192.168.69.166'
+  'hotel'       'uav67' '192.168.69.167'
+  'india'       'uav68' '192.168.69.168'
+  'kilo'        'uav69' '192.168.69.169'
+  'lima'        'uav70' '192.168.69.170'
+  'mike'        'uav71' '192.168.69.171'
+  'dofec'       'uav80' '192.168.69.180'
+  'juliett'     'uav90' '192.168.69.190'
+  'eagle'       'uav91' '192.168.69.191'
+  'xavnx1'      'uav95' '192.168.69.195'
+  'xavier'      'uav99' '192.168.69.199'
+
+  # our laptops, remove if needed
+
+  'klaxalk' 'klaxalk-local' '192.168.69.11'
+  'dan'     'dan-local'     '192.168.69.42'
 )
 
 # get path to script
@@ -82,7 +85,8 @@ done
 hostname=( "${hostname[@]}" "${nato[@]}" )
 ip=( "${ip[@]}" "${ip[@]}" )
 
-my_hostname=$( cat /etc/hostname )
+# my_hostname=$( cat /etc/hostname )
+my_hostname=uav15
 
 for ((i=0; i < ${#hostname[*]}; i++)); do
 
@@ -109,7 +113,7 @@ for ((i=0; i < ${#hostname[*]}; i++)); do
 " >> ~/.ssh/config
 
   # move the current entry to the bottom of the file
-  $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/${ip[i]}.*/norm dapGp" -c "wqa" -- "$HOME/.ssh/config"
+  $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/${ip[i]}\s.*/norm dapGp" -c "wqa" -- "$HOME/.ssh/config"
 
   num=`cat /etc/hosts | grep ".* ${hostname[i]}$" | wc -l`
   if [ "$num" -lt "1" ]; then
@@ -120,7 +124,7 @@ for ((i=0; i < ${#hostname[*]}; i++)); do
 
     # delete the old entry
     echo "deleting old entry in /etc/hosts"
-    sudo $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/.*${hostname[i]}$/norm dd" -c "wqa" -- "/etc/hosts"
+    sudo $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/.*\s${hostname[i]}$/norm dd" -c "wqa" -- "/etc/hosts"
 
   fi
 
@@ -132,7 +136,7 @@ for ((i=0; i < ${#hostname[*]}; i++)); do
 
     sudo bash -c "echo ${ip[i]} ${hostname[i]} >> /etc/hosts"
 
-    sudo $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/${ip[i]}/norm ddGp" -c "wqa" -- "/etc/hosts"
+    sudo $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "%g/${ip[i]}\s/norm ddGp" -c "wqa" -- "/etc/hosts"
     sudo $VIM_BIN $HEADLESS -Ens -c "set ignorecase" -c "normal Go" -c "wqa" -- "/etc/hosts"
 
   fi
