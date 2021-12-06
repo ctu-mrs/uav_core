ce11691460dcc49a42a0c9ff9c79dda82c158f00
Pavel Petracek <petrapa6@fel.cvut.cz>
2021-12-06 11:28:47 +0100

    scope_timer_viewer: update with plotting functionality

--- miscellaneous/scope_timer_viewer/scope_timer_viewer.py
+++ miscellaneous/scope_timer_viewer/scope_timer_viewer.py
@@ -5,6 +5,9 @@ import os
 import sys
 import numpy as np
 
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
 # # #{ print_help
 def print_help():
     print('From scope timer logs (.log/.csv/.txt; format: [scope,label_from,label_to,sec_start,sec_end,sec_duration]), print out and plot the timers.')
@@ -14,15 +17,16 @@ def print_help():
     print('C) ./scope_timer_viewer.py first_log.log second_log.log [..] -> process both logs and compare them')
     print('D) ./scope_timer_viewer.py first_dir second_dir [..]         -> process both directories and compare all logs')
     print('Optional parameters:')
-    print('--scope scope_name (string, default: all scopes) -> only scope with name \"scope_name\" will be processed')
-    print('--checkpoints enabled (bool, default: False) -> all checkpoints within scopes will be processed')
+    print('--scope scope_name (string, default: "" (all scopes)) -> only scope with name \"scope_name\" will be processed')
+    print('--checkpoints (bool flag)                             -> all checkpoints within scopes will be processed')
+    print('--plot (bool flag)                                    -> data will be plotted')
 # # #}
 
 # # #{ load_params
 def load_params(args):
     path_A = args[1]
 
-    if path_A in ['-h', '--h', '--help', 'help', '-help']:
+    if path_A in ('-h', '--h', '--help', 'help', '-help'):
         print_help()
         exit(0)
 
@@ -30,6 +34,7 @@ def load_params(args):
     path_B      = None
     scope       = None
     checkpoints = False
+    plot        = False
 
     # It's ugly but it works somehow (am slightly drunk, woke up at 4AM, and am writing this in the plane without internet)
     argc = len(args)
@@ -43,10 +48,12 @@ def load_params(args):
         for i in range(start_idx, argc):
             if args[i] == '--scope' and i + 1 < argc:
                 scope = str(args[i+1])
-            elif args[i] == '--checkpoints' and i + 1 < argc:
-                checkpoints = True if str(args[i+1]) in ['true', 'True'] else False
+            elif args[i] == '--checkpoints':
+                checkpoints = True
+            elif args[i] == '--plot':
+                plot = True
 
-    return path_A, path_B, scope, checkpoints
+    return path_A, path_B, scope, checkpoints, plot
 # # #}
 
 # # #{ DataLoader class
@@ -61,9 +68,10 @@ class DataLoader:
     SEC_DURATION_MEAN='sec_duration_mean'
     SEC_DURATION_STD='sec_duration_std'
     DATA='data'
-    SUPPORTED_FORMATS=['.log', '.txt', '.csv']
+    SUPPORTED_FORMATS=('.log', '.txt', '.csv')
 
-    data = (None, None)
+    data             = (None, None)
+    loading_from_dir = False
 
 # # #{ load_file
     def load_file(self, filepath):
@@ -106,6 +114,7 @@ class DataLoader:
 
 # # #{ load_dir
     def load_dir(self, dirpath):
+        self.loading_from_dir = True
         data = {}
 
         for filename in os.listdir(dirpath):
@@ -119,7 +128,28 @@ class DataLoader:
                 continue
 
             # Merge dicts (careful, filedata values override data values if a key is present in both dicts)
-            data = {**data, **filedata}
+            # data = {**data, **filedata}
+            
+            # Merge dicts
+            for key_scope in filedata:
+
+                if not key_scope in data:
+                    data[key_scope] = filedata[key_scope]
+                    continue
+
+                for key_label_from in filedata[key_scope]:
+
+                    if not key_label_from in data[key_scope]:
+                        data[key_scope][key_label_from] = filedata[key_scope][key_label_from]
+                        continue
+
+                    for key_label_to in filedata[key_scope][key_label_from]:
+
+                        if not key_label_to in data[key_scope][key_label_from]:
+                            data[key_scope][key_label_from][key_label_to]            = {}
+                            data[key_scope][key_label_from][key_label_to][self.DATA] = filedata[key_scope][key_label_from][key_label_to][self.DATA]
+                        else:
+                            data[key_scope][key_label_from][key_label_to][self.DATA].extend(filedata[key_scope][key_label_from][key_label_to][self.DATA])
 
         if len(data.keys()) == 0:
             return None
@@ -141,6 +171,8 @@ class DataLoader:
 
 # # #{ load_dirs
     def load_dirs(self, dirpath_A, dirpath_B):
+        self.loading_from_dir = True
+
         dirdata_A = self.load_dir(dirpath_A)
         dirdata_B = self.load_dir(dirpath_B)
 
@@ -234,6 +266,7 @@ def filter_data(data_loader, scope=None, checkpoints=False):
 def process_data(data_loader, scope=None, checkpoints=False):
 
     data = filter_data(data_loader, scope, checkpoints)
+        
 
     for data_idx in (0, 1):
 
@@ -304,9 +337,74 @@ def print_data(data_loader, path_A, path_B):
                     
 # # #}
 
+# # #{ plot_data
+def plot_data(data_loader, path_A, path_B):
+
+    # if data_loader.loading_from_dir:
+    #     print('[plot_data] Entire directories will not be plotted (may contain files with same scopes but varying timestamps). Rerun for single file or file-to-file comparison.')
+    #     return
+    
+    if data_loader.data[1] is None:
+
+        for key_scope in data_loader.data[0]:
+            for key_label_from in data_loader.data[0][key_scope]:
+                for key_label_to in data_loader.data[0][key_scope][key_label_from]:
+                    secs_from = [element[data_loader.SEC_FROM] for element in data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.DATA]]
+                    secs_to   = [element[data_loader.SEC_TO] for element in data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.DATA]]
+                    durations = [element[data_loader.SEC_DURATION] for element in data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.DATA]]
+                    duration_mean_ms = data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.SEC_DURATION_MEAN] * 1000.0
+
+                    fig = plt.figure()
+                    ax = fig.add_subplot(111)
+                    label=key_scope + ' (mean={:.1f} ms)'.format(duration_mean_ms)
+                    ax.plot(durations, color='red', label=label)
+                    ax.legend()
+                    
+                    plt.show(block=True)
+
+    else:
+
+        for key_scope in data_loader.data[0]:
+            if key_scope not in data_loader.data[1]:
+                continue
+
+            for key_label_from in data_loader.data[0][key_scope]:
+                if key_label_from not in data_loader.data[1][key_scope]:
+                    continue
+
+                for key_label_to in data_loader.data[0][key_scope][key_label_from]:
+                    if key_label_to not in data_loader.data[1][key_scope][key_label_from]:
+                        continue
+                    
+                    A_durations = [element[data_loader.SEC_DURATION] for element in data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.DATA]]
+                    B_durations = [element[data_loader.SEC_DURATION] for element in data_loader.data[1][key_scope][key_label_from][key_label_to][data_loader.DATA]]
+                    
+                    convolve_N = 20
+                    A_durations_convolved = np.convolve(A_durations, np.ones(convolve_N)/convolve_N, mode='valid')
+                    B_durations_convolved = np.convolve(B_durations, np.ones(convolve_N)/convolve_N, mode='valid')
+
+                    A_duration_mean_ms = data_loader.data[0][key_scope][key_label_from][key_label_to][data_loader.SEC_DURATION_MEAN] * 1000.0
+                    B_duration_mean_ms = data_loader.data[1][key_scope][key_label_from][key_label_to][data_loader.SEC_DURATION_MEAN] * 1000.0
+
+                    fig = plt.figure()
+                    ax = fig.add_subplot(111)
+                    A_label=key_scope + ' (mean={:.1f} ms)'.format(A_duration_mean_ms)
+                    B_label=key_scope + ' (mean={:.1f} ms)'.format(B_duration_mean_ms)
+                    ax.plot(A_durations, color='red', alpha=0.15)
+                    ax.plot(B_durations, color='blue', alpha=0.15)
+                    ax.plot(A_durations_convolved, label=A_label, color='red')
+                    ax.plot(B_durations_convolved, label=B_label, color='blue')
+                    plt.xlabel('Sample (-)')
+                    plt.ylabel('Time (s)')
+                    ax.legend()
+                    
+                    plt.show(block=True)
+                    
+# # #}
+
 if __name__ == "__main__":
     
-    path_A, path_B, scope, checkpoints = load_params(sys.argv)
+    path_A, path_B, scope, checkpoints, plot = load_params(sys.argv)
     
     # Load data
     loader = DataLoader()
@@ -319,3 +417,6 @@ if __name__ == "__main__":
 
     process_data(loader, scope=scope, checkpoints=checkpoints)
     print_data(loader, path_A, path_B)
+
+    if plot:
+        plot_data(loader, path_A, path_B)
