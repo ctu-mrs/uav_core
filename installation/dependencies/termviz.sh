313b2e767d34b73c0436848d63eae79227da5576
Viktor Walter <viktor.walter@disroot.org>
2023-04-12 13:57:47 +0200

    Attempting to fix build issue

--- installation/dependencies/termviz.sh
+++ installation/dependencies/termviz.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+distro=`lsb_release -r | awk '{ print $2 }'`
+[ "$distro" = "18.04" ] && ROS_DISTRO="melodic"
+[ "$distro" = "20.04" ] && ROS_DISTRO="noetic"
+
 # get the path to this script
 MY_PATH=`dirname "$0"`
 MY_PATH=`( cd "$MY_PATH" && pwd )`
+
+echo "Installing termviz"
 
 sudo apt-get -y install cargo
 
@@ -10,8 +16,10 @@ sudo apt-get -y install cargo
 export ROS_WORKSPACES_STORE=$ROS_WORKSPACES
 export ROS_WORKSPACES=""
 echo "Re-sourcing ROS setup"
-source /opt/ros/*/setup.bash
+source /opt/ros/${ROS_DISTRO}/setup.bash
 cd $MY_PATH/../../utils/termviz
+
+echo "Running cargo build on termviz"
 cargo build --release
 export ROS_WORKSPACES=$ROS_WORKSPACES_STORE
 # source $HOME/mrs_workspace/devel/setup.bash
