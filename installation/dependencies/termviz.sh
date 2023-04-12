5a86ca352899855f95419a8fe5ab31b8ed74c743
Viktor Walter <viktor.walter@disroot.org>
2023-04-12 15:29:40 +0200

    Adding catkin_ignore to termviz

--- installation/dependencies/termviz.sh
+++ installation/dependencies/termviz.sh
@@ -18,9 +18,9 @@ export ROS_WORKSPACES=""
 echo "Re-sourcing ROS setup"
 source /opt/ros/${ROS_DISTRO}/setup.bash
 cd $MY_PATH/../../utils/termviz
-
 echo "Running cargo build on termviz"
 cargo build --release
+touch ./CATKIN_IGNORE
 export ROS_WORKSPACES=$ROS_WORKSPACES_STORE
 # source $HOME/mrs_workspace/devel/setup.bash
 
